2b299a8c85757d20ea379a88f303d1a4f23c60ea
jaymark22 <62195744+jaymark22@users.noreply.github.com>
2022-08-10 23:12:39 +0800

    Add files via upload

---
+++ Find_Angle_MBC_HackerRank.py
@@ -0,0 +1,9 @@
+import math
+
+SideA = int(input("SideA: "))
+SideB = int(input("SideB: "))
+SideC = math.sqrt((SideA**2)+(SideB**2))
+Distance = (SideC/2)
+AngleC =  math.degrees(math.asinh(SideA/SideB))
+FinalAnswer =
+
