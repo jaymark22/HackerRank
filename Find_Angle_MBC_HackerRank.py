9059b3fe94ef196b98e81c9dff4778c05ad1564d
jaymark22 <62195744+jaymark22@users.noreply.github.com>
2022-08-13 05:04:56 +0800

    Delete Find_Angle_MBC_HackerRank.py

--- Find_Angle_MBC_HackerRank.py
+++
@@ -1,9 +0,0 @@
-import math
-
-SideA = int(input("SideA: "))
-SideB = int(input("SideB: "))
-SideC = math.sqrt((SideA**2)+(SideB**2))
-Distance = (SideC/2)
-AngleC =  math.degrees(math.asinh(SideA/SideB))
-FinalAnswer =
-
